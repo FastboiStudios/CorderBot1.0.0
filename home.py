1e493cc5b1d2c13243dc43e8c912e365926758ce
FastboiStudios <121701084+FastboiStudios@users.noreply.github.com>
2023-08-12 17:42:07 -0400

    Update home.py

--- home.py
+++ home.py
@@ -1,2 +1,9 @@
 import os
 os.remove("installer.py")
+
+addon_Cmd = input ('Add addon url: http://')
+
+if addon_Cmd == ('team-crew.github.io/'):
+ pass
+ print  ('booting THE CREW')
+ open ('index.html')
