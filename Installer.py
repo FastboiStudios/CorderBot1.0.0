ba09cb4b8cb27eb259f537af1d1022d9708105da
FastboiStudios <121701084+FastboiStudios@users.noreply.github.com>
2023-08-12 17:25:54 -0400

    Installer.py

---
+++ Installer.py
@@ -0,0 +1,28 @@
+print ('Installing disk...')
+
+f = open("Addons.js", "a")
+f.write("")
+f.close()
+
+#open and read the file after the appending:
+f = open("Addons.js", "r")
+print(f.read())
+
+print ('Loading: Gboard-the-google-Keyboard_13.3.04.55153865-bata-arm6...')
+
+def kodichange:
+ pass
+print (Downloading kodi update...')
+password = input ('Make a custom password or press enter for no password: ')
+
+f = open("password.py", "a")
+f.write(password)
+f.close()
+
+#open and read the file after the appending:
+f = open("password.py", "r")
+print(f.read())
+
+print ('Downloading password maniger...')
+
+open ('Home.py')
